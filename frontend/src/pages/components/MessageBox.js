7d5b7cd326658e473dffc37682f34abb9adaccf7
Lisa Wilton <liswil05@yahoo.co.nz>
2022-09-10 11:39:27 +1200

    removed files to start again

--- frontend/src/pages/components/MessageBox.js
+++
@@ -1,8 +0,0 @@
-//code by Jacynta
-//code inspired by "React & Node ECommerce Tutorials for Beginners 2022 [MERN Stack ECommerce Website]" tutorial by Coding with Basir on YouTube
-
-import Alert from 'react-bootstrap/Alert';
-
-export default function MessageBox(props) {
-  return <Alert variant={props.variant || 'info'}>{props.children}</Alert>;
-}
--- frontend/src/utils.js
+++
@@ -1,8 +0,0 @@
-//code by Jacynta
-//code inspired by "React & Node ECommerce Tutorials for Beginners 2022 [MERN Stack ECommerce Website]" tutorial by Coding with Basir on YouTube
-
-export const getError = (error) => {
-  return error.response && error.response.data.message
-    ? error.response.data.message
-    : error.message;
-};
