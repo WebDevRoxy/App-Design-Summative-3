475f736ac10f4cf9c673c77ee072cecf8f755b5e
Lisa Wilton <liswil05@yahoo.co.nz>
2022-09-10 14:06:09 +1200

    added images folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -69,7 +69,7 @@ function App() {
           </Navbar>
         </header>
         <main>
-          //have a look at this
+          <h1>Products</h1>
           <Container className="mt-3">
             {categories.map((category) => (
               <Nav.Item key={category}>
