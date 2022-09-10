7ee340ed38242ad473f73b27e2c4bb78bf305c02
Lisa Wilton <liswil05@yahoo.co.nz>
2022-09-10 19:18:05 +1200

    added product formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,9 +11,18 @@ function App() {
         <div className="products">
           {data.products.map((product) => (
             <div className="product" key={product.slug}>
-              <img src={product.image} alt={product.name} />
-              <p>{product.name}</p>
-              <p>{product.price}</p>
+              <a href={"/product/${product.slug}"}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={"/product/${product.slug}"}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
             </div>
           ))}
         </div>
