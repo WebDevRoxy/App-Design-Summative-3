0513d2d8ec698d77dce263244fa7a1ecfc35b5d7
liswil05 <111119774+liswil05@users.noreply.github.com>
2022-09-19 11:21:47 +1200

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+//code by Jacynta
+//code inspired by "React & Node ECommerce Tutorials for Beginners 2022 [MERN Stack ECommerce Website]" tutorial by Coding with Basir on YouTube
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals.js';
+import { StoreProvider } from './pages/Store';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StoreProvider>
+      <HelmetProvider>
+        <PayPalScriptProvider deferLoading={true}>
+          <App />
+        </PayPalScriptProvider>
+      </HelmetProvider>
+    </StoreProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
