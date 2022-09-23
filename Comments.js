2b8491f5a0584cf236e5f1c7406d259babe726c2
liswil05 <111119774+liswil05@users.noreply.github.com>
2022-09-24 10:06:23 +1200

    Create Comments.js

---
+++ Comments.js
@@ -0,0 +1,26 @@
+//Lisa Code
+
+import mongoose, { Schema } from 'mongoose';
+
+const commentSchema = new mongoose.Schema(
+  {
+    writer: { type: Schema.Types.ObjectId, ref: 'User' },
+    postId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Listing',
+    },
+    responseTo: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    content: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Comment = mongoose.model('Comment', commentSchema);
+export default Comment;
