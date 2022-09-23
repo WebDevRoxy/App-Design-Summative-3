0ace523f19b10ad1a6004b1b72885cc20079661a
liswil05 <111119774+liswil05@users.noreply.github.com>
2022-09-24 10:09:22 +1200

    Update Comments.js

--- Comments.js
+++ Comments.js
@@ -1,26 +1,53 @@
-//Lisa Code
+//Lisa code
 
-import mongoose, { Schema } from 'mongoose';
+import { Input } from '@material-ui/core';
+import axios from 'axios';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import { useSelector } from 'react-redux';
+const { TextArea } = Input;
 
-const commentSchema = new mongoose.Schema(
-  {
-    writer: { type: Schema.Types.ObjectId, ref: 'User' },
-    postId: {
-      type: Schema.Types.ObjectId,
-      ref: 'Listing',
-    },
-    responseTo: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-    },
-    content: {
-      type: String,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+function Comments() {
+  const user = useSelector((state) => state.user);
+  const [Comment, setComment] = useState('');
+  const handleChange = (e) => {
+    setComment(e.currentTarget.value);
+  };
 
-const Comment = mongoose.model('Comment', commentSchema);
-export default Comment;
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const variables = {
+      content: Comment,
+      writer: user.userData._id,
+    };
+
+    axios.post('/api/comment/saveComment', variables);
+  };
+
+  return (
+    <div>
+      <br />
+      <p> replies</p>
+      <hr />
+
+      {/* Comment Lists*/}
+
+      {/* Root Comment Form */}
+
+      <form style={{ display: 'flex' }} onSubmit={onSubmit}>
+        <TextArea
+          style={{ width: '100%', borderRadius: '5px' }}
+          onChange={handleChange}
+          value={Comment}
+          placeholder="write some comments"
+        />
+        <br />
+        <Button style={{ width: '20%', height: '52px' }}>Submit</Button>
+      </form>
+    </div>
+  );
+}
+
+export default Comments;
+
